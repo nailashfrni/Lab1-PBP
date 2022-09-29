38a73235f4dbf779078a1f5bbe6bc1d14c050875
nailashafrni <nailashafirni2003@gmail.com>
2022-09-29 15:46:31 +0700

    Lab 4

--- wishlist/views.py
+++ wishlist/views.py
@@ -18,7 +18,7 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
         'list_barang': data_barang_wishlist,
-        'nama': 'Naila',
+        'nama': request.user,
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
